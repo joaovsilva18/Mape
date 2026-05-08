08987805e4d5769e39b0031394c36b69f275cc53
joaovsilva18 <joaovactorsilva@gmail.com>
2026-05-08 00:19:52 -0300

    feat: add pastas de roteamento

---
+++ src/app/empresas/page.js
@@ -0,0 +1,8 @@
+export default function EmpresasPage() {
+  return (
+    <div>
+      <h1>Página de Empresas</h1>
+      <p>Gerenciamento de empresas parceiras.</p>
+    </div>
+  );
+}
---
+++ src/app/login/page.js
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <main>
+      <h1>Página de Login</h1>
+    </main>
+  );
+}
